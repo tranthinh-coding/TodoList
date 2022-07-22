f36956688cb8e99397f0246ed32f83c723cab935
Thinh <73821256+tranthinh-coding@users.noreply.github.com>
2022-07-22 20:13:00 +0700

    ➕ Create get csrf_token for test API

--- server/routes/web.php
+++ server/routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', static function () {
+//    return view('auth.register');
+//});
+
+Route::get('/_token', static function() {
+    echo csrf_token();
 });
