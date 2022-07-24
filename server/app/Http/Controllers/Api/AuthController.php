c1a1f8b99ff4581cc9f55470bbbf4bd2738603b1
Thinh <73821256+tranthinh-coding@users.noreply.github.com>
2022-07-24 12:54:00 +0700

    ✅✏️Handle login register logout

--- server/app/Http/Controllers/Api/AuthController.php
+++ server/app/Http/Controllers/Api/AuthController.php
@@ -5,35 +5,88 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
-use App\Http\Requests\SignInRequest;
-use App\Http\Requests\SignUpRequest;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
+use App\Models\PersonalAccessToken;
 
 class AuthController extends Controller {
-    public function handleSignIn (SignInRequest $request): JsonResponse {
-        return response()->json('done', 200);
+    private function jsonResponse (array $data, int $status = 200, array $headers = [], int $options = 0): JsonResponse {
+        return response()->json($data, $status, $headers, $options);
     }
 
-    public function handleSignOut (Request $request): JsonResponse {
-        return response()->json('done', 200);
+    /**
+     * @param LoginRequest $request
+     *
+     * @return JsonResponse
+     */
+    public function handleLogin (LoginRequest $request): JsonResponse {
+        $user = User::query()
+                    ->where('email', $request->get('email'))
+                    ->firstOrFail();
+        if (! $user || ! Hash::check($request->get('password'), $user['password'])) {
+            return $this->jsonResponse(['This email or password is incorrect']);
+        }
+        $token = $user->createToken('authToken')->plainTextToken;
+        return $this->jsonResponse([
+            'message'      => 'Login Success',
+            'access_token' => $token,
+            'token_type'   => 'Bearer',
+            'status_code'  => 200,
+            'user'         => $user,
+        ]);
     }
 
-    public function handleSignUp (SignUpRequest $request): JsonResponse {
-        $userData = [
-            'name'           => $request['name'],
-            'email'          => $request['email'],
-            'password'       => Hash::make($request['password']),
-            'remember_token' => Str::random(10),
-        ];
+    /**
+     * @param Request $request
+     *
+     * @return JsonResponse
+     */
+    public function handleLogout (Request $request): JsonResponse {
+        // merge token if array length < 2 => $token cannot assign
+        [$tokenId, $token] = array_merge(explode('|', $request->bearerToken()), ['']);
+        if (!$tokenId) {
+            return $this->jsonResponse(['message' => 'You\'re not login']);
+        }
 
-        $inserted = DB::table('users')->insert($userData);
+        $userId = PersonalAccessToken::getUserId($tokenId, $token);
+        if (! $userId) {
+            return $this->jsonResponse([
+                'message' => 'Token is invalid',
+            ]);
+        }
 
-        $message = $inserted ? 'Sign Up Successfully' : 'Cannot Sign Up';
+        PersonalAccessToken::removeAllTokenUser($userId);
+        return $this->jsonResponse([
+            'message' => 'You have been logged out',
+        ]);
+    }
 
-        return response()->json([
-            'message' => $message,
+    /**
+     * @param RegisterRequest $request
+     *
+     * @return JsonResponse
+     */
+    public function handleRegister (RegisterRequest $request): JsonResponse {
+        $remember_token = $request->get('remember') ? Str::random(10) : NULL;
+        $user = User::query()->create([
+            'name'           => $request->get('name'),
+            'email'          => $request->get('email'),
+            'password'       => Hash::make($request->get('password')),
+            'remember_token' => $remember_token,
+        ]);
+        if (!$user) {
+            return $this->jsonResponse(['Register failed'], 406);
+        }
+        $token = $user->createToken('authToken')->plainTextToken;
+        return $this->jsonResponse([
+            'message'      => 'Register Success',
+            'access_token' => $token,
+            'token_type'   => 'Bearer',
+            'status_code'  => 201,
+            'user'         => $user,
         ], 201);
     }
 }
