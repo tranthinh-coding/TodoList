1b1360707c1f8c0be8285faa88b46d8905bdb70f
Thinh <73821256+tranthinh-coding@users.noreply.github.com>
2022-07-26 09:33:41 +0700

    🔄️remove labelId, change to label_name,color

--- server/database/factories/TaskFactory.php
+++ server/database/factories/TaskFactory.php
@@ -7,15 +7,38 @@ use App\Enums\TaskStatusEnum;
 use Illuminate\Support\Facades\DB;
 
 class TaskFactory extends Factory {
+    /**
+     * @throws \Exception
+     */
     public function definition (): array {
-        $user_id  = DB::table('users')->pluck('id')->random();
-        $label_id = DB::table('task_labels')->pluck('id')->random();
+        $user_id = DB::table('users')->pluck('id')->random(1);
+
+        $names  = [
+            'Gia đình',
+            'Công việc',
+            'Khách hàng',
+            'Trả lời sau',
+            'Đồng nghiệp',
+        ];
+        $colors = [
+            '#fbbf24',
+            '#f87171',
+            '#84cc16',
+            '#22c55e',
+            '#2dd4bf',
+            '#38bdf8',
+            '#a78bfa',
+            '#e879f9',
+            '#f472b6',
+            '#f43f5e',
+        ];
 
         return [
-            'name'     => $this->faker->jobTitle(),
-            'status'   => TaskStatusEnum::getRandomValue(),
-            'user_id'  => $user_id,
-            'label_id' => $label_id,
+            'name'        => $this->faker->jobTitle(),
+            'status'      => TaskStatusEnum::getRandomValue(),
+            'user_id'     => $user_id,
+            'label_name'  => $this->faker->randomElement($names),
+            'label_color' => $this->faker->randomElement($colors),
         ];
     }
 }
