3f8255b0d5fcd5a12a62e0a2b1d91d427859a14e
Thinh <73821256+tranthinh-coding@users.noreply.github.com>
2022-07-27 00:54:06 +0700

    ✏️Add all properties to TYPES[]

--- server/app/Enums/TaskStatusEnum.php
+++ server/app/Enums/TaskStatusEnum.php
@@ -5,10 +5,10 @@ namespace App\Enums;
 use BenSampo\Enum\Enum;
 
 final class TaskStatusEnum extends Enum {
-
     /** DONE     : Task was completed */
-    public const DONE    = "Done";
-
+    public const DONE = "Done";
     /** PENDING  : Task is pending */
     public const PENDING = "Pending";
+    /** @var string[] ALL TYPES OF ENUM */
+    public const TYPES = [self::DONE, self::PENDING];
 }
