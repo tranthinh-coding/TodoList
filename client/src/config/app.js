d331211fc4c100e38324ce1af139114a2935b738
Thinh <73821256+tranthinh-coding@users.noreply.github.com>
2022-07-28 13:29:30 +0700

    edit some config app

--- client/src/config/app.js
+++ client/src/config/app.js
@@ -1,5 +1,7 @@
 export const API_V1 = process.env.API_V1;
-export const TokenKey = "access_token";
+export const ACCESS_TOKEN = "access_token";
+export const REMEMBER_TOKEN = "remember_token"; // remember_token
 export const REGISTER_API = "/register";
 export const LOGIN_API = "/login";
 export const LOGOUT_API = "/logout";
+export const nameOf = (f) => (f).toString().replace(/[ |\(\)=>]/g,''); // name of variable
