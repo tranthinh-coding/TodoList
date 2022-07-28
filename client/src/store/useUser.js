a68059a25ee0a3e102c145352f81df46e7b747dd
Thinh <73821256+tranthinh-coding@users.noreply.github.com>
2022-07-28 13:32:11 +0700

    Edit method login register user

--- client/src/store/useUser.js
+++ client/src/store/useUser.js
@@ -1,26 +1,90 @@
 import { defineStore } from "pinia";
-// import request from "@/utils/request";
 import { useFetch } from "@/composables/useFetch";
 
-import { TokenKey } from "@/config/app";
-import { removeToken } from "@/utils/auth";
+import {
+  LOGIN_API,
+  REGISTER_API,
+  ACCESS_TOKEN,
+  REMEMBER_TOKEN,
+} from "@/config/app";
+import { removeToken, setToken } from "@/utils/auth";
+import { ElMessage } from "element-plus";
+import i18n from "@/config/i18n";
 
 export const useUser = defineStore("user", {
   state: () => ({
     name: "",
     email: "",
-    [TokenKey]: "",
+    [ACCESS_TOKEN]: "",
     loginStatus: false,
   }),
   actions: {
-    logout() {
-      return removeToken();
+    async login(form) {
+      const { data, error, isFetching } = await useFetch(LOGIN_API, {
+        method: "post",
+      })
+        .post(form)
+        .json();
+
+      if (error.value) {
+        typeof error.value == "string" &&
+          ElMessage({
+            message: error.value,
+            type: "error",
+            duration: 5 * 1000,
+            showClose: true,
+          });
+      } else {
+        ElMessage({
+          message: i18n.global.t("auth.loginSuccess"),
+          type: "success",
+          duration: 5 * 1000,
+          showClose: true,
+        });
+
+        console.log(data.value);
+        this.saveUser(data.value);
+        setToken(ACCESS_TOKEN, this[ACCESS_TOKEN]);
+        setToken(REMEMBER_TOKEN, data.value[REMEMBER_TOKEN]);
+      }
+      return { data, error, isFetching };
     },
-    saveUser({ name, email, [TokenKey]: token, loginStatus }) {
+    async register(form) {
+      const { data, error, isFetching } = await useFetch(REGISTER_API, {
+        method: "post",
+      })
+        .post(form)
+        .json();
+
+      if (error.value) {
+        typeof error.value == "string" &&
+          ElMessage({
+            message: error.value,
+            type: "error",
+            duration: 5 * 1000,
+            showClose: true,
+          });
+      } else {
+        ElMessage({
+          message: i18n.global.t("auth.registerSuccess"),
+          type: "success",
+          duration: 5 * 1000,
+          showClose: true,
+        });
+        this.saveUser(data.value);
+        setToken(ACCESS_TOKEN, this[ACCESS_TOKEN]);
+      }
+      return { data, error, isFetching };
+    },
+    logout() {
+      this.$reset();
+      return removeToken([ACCESS_TOKEN, REMEMBER_TOKEN]);
+    },
+    saveUser({ name, email, [ACCESS_TOKEN]: token }) {
       this.name = name;
       this.email = email;
-      this[TokenKey] = token;
-      this.loginStatus = loginStatus;
+      this[ACCESS_TOKEN] = token;
+      this.loginStatus = true;
     },
   },
   getters: {
