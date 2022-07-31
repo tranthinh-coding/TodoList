d4f2d45c10eb21f251c8b9c4a8dc861d05467cf7
Thinh <73821256+tranthinh-coding@users.noreply.github.com>
2022-07-31 23:59:24 +0700

    make lang for i18n

--- client/src/lang/enUS.js
+++ client/src/lang/enUS.js
@@ -1,103 +1,122 @@
 export default {
-  auth: {
-    login: "Login",
-    logout: "Logout",
-    register: "Register",
-    loginError: "Login failed",
-    loginSuccess: "Login successfully",
-    logoutError: "Logout failed",
-    logoutSuccess: "You have been logout",
-    registerError: "Register failed",
-    registerSuccess: "Register successfully",
+  auth       : {
+    login           : "Login",
+    logout          : "Logout",
+    register        : "Register",
+    loginError      : "Login failed",
+    loginSuccess    : "Login successfully",
+    logoutError     : "Logout failed",
+    logoutSuccess   : "You have been logout",
+    registerError   : "Register failed",
+    registerSuccess : "Register successfully",
+    haveAnAccount   : "Have an account?",
+    haveNotAnAccount: "Haven't an account?"
   },
-  pages: {
-    home: "Home",
-    welcomeBack: 'Welcome back',
-    getStarted: 'Get started'
+  pages      : {
+    home       : "Home",
+    welcomeBack: "Welcome back",
+    getStarted : "Get started",
+    noTasks    : "There are no tasks here!"
   },
-  form: {
-    name: "Full name",
-    email: "Email",
-    password: "Password",
+  carousel   : {
+    prev     : "Previous visual",
+    next     : "Next visual",
+    ariaLabel: {
+      delimiter: "Carousel slide {0} of {1}"
+    }
+  },
+  rating     : {
+    ariaLabel: {
+      icon: "Rating {0} of {1}"
+    }
+  },
+  form       : {
+    name                : "Full name",
+    email               : "Email",
+    password            : "Password",
     passwordConfirmation: "Password confirmation",
-    rememberMe: "Remember me",
+    rememberMe          : "Remember me"
   },
-  validate: {
-    required: "This field must be require",
-    email: "Please input correct email address",
-    min: "This field must be atleast {0} character | This field must be atleast {1} character",
-    confirmation: "Confirmation {type} is incorrect",
+  validate   : {
+    required    : "This field must be require",
+    email       : "Please input correct email address",
+    min         : "This field must be at least {0} character | This field must be at least {1} character",
+    confirmation: "Confirmation {type} is incorrect"
   },
-  network: {
+  network    : {
     connectSuccess: "Stable internet connection",
     connectFailure: "Internet connection failed",
-    connected: "Internet connected",
-    connecting: "Connecting",
-    reconnecting: "Re-@:network.connecting",
-    reconnected: "Internet connection restored",
-    disconnected: "Disconnect",
-    offline: "You are currently offline",
+    connected     : "Internet connected",
+    connecting    : "Connecting",
+    reconnecting  : "Re-@:network.connecting",
+    reconnected   : "Internet connection restored",
+    disconnected  : "Disconnect",
+    offline       : "You are currently offline"
   },
   colorpicker: {
-    confirm: "OK",
-    clear: "Clear",
+    confirm     : "OK",
+    clear       : "Clear",
     defaultLabel: "color picker",
-    description: "current color is {color}. press enter to select a new color.",
+    description : "current color is {color}. press enter to select a new color."
   },
-  datepicker: {
-    now: "Now",
-    today: "Today",
-    cancel: "Cancel",
-    clear: "Clear",
-    confirm: "OK",
-    dateTablePrompt:
+  timePicker : {
+    am: "AM",
+    pm: "PM"
+  },
+  datepicker : {
+    now             : "Now",
+    today           : "Today",
+    cancel          : "Cancel",
+    clear           : "Clear",
+    confirm         : "OK",
+    dateTablePrompt :
       "Use the arrow keys and enter to select the day of the month",
     monthTablePrompt: "Use the arrow keys and enter to select the month",
-    yearTablePrompt: "Use the arrow keys and enter to select the year",
-    selectedDate: "Selected date",
-    selectDate: "Select date",
-    selectTime: "Select time",
-    startDate: "Start Date",
-    startTime: "Start Time",
-    endDate: "End Date",
-    endTime: "End Time",
-    prevYear: "Previous Year",
-    nextYear: "Next Year",
-    prevMonth: "Previous Month",
-    nextMonth: "Next Month",
-    year: "",
-    month1: "January",
-    month2: "February",
-    month3: "March",
-    month4: "April",
-    month5: "May",
-    month6: "June",
-    month7: "July",
-    month8: "August",
-    month9: "September",
-    month10: "October",
-    month11: "November",
-    month12: "December",
-    week: "week",
-    weeks: {
+    yearTablePrompt : "Use the arrow keys and enter to select the year",
+    selectedDate    : "Selected date",
+    selectDate      : "Select date",
+    selectTime      : "Select time",
+    startDate       : "Start Date",
+    startTime       : "Start Time",
+    endDate         : "End Date",
+    endTime         : "End Time",
+    prevYear        : "Previous Year",
+    nextYear        : "Next Year",
+    prevMonth       : "Previous Month",
+    nextMonth       : "Next Month",
+    year            : "",
+    month1          : "January",
+    month2          : "February",
+    month3          : "March",
+    month4          : "April",
+    month5          : "May",
+    month6          : "June",
+    month7          : "July",
+    month8          : "August",
+    month9          : "September",
+    month10         : "October",
+    month11         : "November",
+    month12         : "December",
+    week            : "week",
+    weeks           : {
       sun: "Sun",
       mon: "Mon",
       tue: "Tue",
       wed: "Wed",
       thu: "Thu",
       fri: "Fri",
-      sat: "Sat",
+      sat: "Sat"
     },
-    weeksFull: {
+    weeksFull       : {
       sun: "Sunday",
       mon: "Monday",
       tue: "Tuesday",
       wed: "Wednesday",
       thu: "Thursday",
       fri: "Friday",
-      sat: "Saturday",
+      sat: "Saturday"
     },
-    months: {
+    months          : {
       jan: "Jan",
       feb: "Feb",
       mar: "Mar",
@@ -109,86 +128,89 @@ export default {
       sep: "Sep",
       oct: "Oct",
       nov: "Nov",
-      dec: "Dec",
-    },
+      dec: "Dec"
+    }
   },
   inputNumber: {
     decrease: "decrease number",
-    increase: "increase number",
+    increase: "increase number"
   },
-  select: {
-    loading: "Loading",
-    noMatch: "No matching data",
-    noData: "No data",
+  select     : {
+    loading    : "Loading",
+    noMatch    : "No matching data",
+    noData     : "No data",
+    placeholder: "Select"
+  },
+  dropdown   : {
+    toggleDropdown: "Toggle Dropdown"
+  },
+  cascader   : {
+    noMatch    : "No matching data",
+    loading    : "Loading",
     placeholder: "Select",
+    noData     : "No data"
   },
-  dropdown: {
-    toggleDropdown: "Toggle Dropdown",
-  },
-  cascader: {
-    noMatch: "No matching data",
-    loading: "Loading",
-    placeholder: "Select",
-    noData: "No data",
-  },
-  pagination: {
-    goto: "Go to",
-    pagesize: "/page",
-    total: "Total {total}",
-    pageClassifier: "",
+  pagination : {
+    goto              : "Go to",
+    pagesize          : "/page",
+    total             : "Total {total}",
+    pageClassifier    : "",
+    currentPage       : "Current Page, Page {0}",
     deprecationWarning:
-      "Deprecated usages detected, please refer to the el-pagination documentation for more details",
+      "Deprecated usages detected, please refer to the el-pagination documentation for more details"
   },
-  dialog: {
-    close: "Close this dialog",
+  dialog     : {
+    close: "Close this dialog"
   },
-  drawer: {
-    close: "Close this dialog",
+  drawer     : {
+    close: "Close this dialog"
   },
-  messagebox: {
-    title: "Message",
+  messagebox : {
+    title  : "Message",
     confirm: "OK",
-    cancel: "Cancel",
-    error: "Illegal input",
-    close: "Close this dialog",
+    cancel : "Cancel",
+    error  : "Illegal input",
+    close  : "Close this dialog"
   },
-  upload: {
-    deleteTip: "press delete to remove",
-    delete: "Delete",
-    preview: "Preview",
-    continue: "Continue",
+  upload     : {
+    deleteTip  : "press delete to remove",
+    delete     : "Delete",
+    preview    : "Preview",
+    continue   : "Continue",
+    counter    : "{0} files",
+    counterSize: "{0} files ({1} in total)"
   },
-  slider: {
-    defaultLabel: "slider between {min} and {max}",
+  slider     : {
+    defaultLabel          : "slider between {min} and {max}",
     defaultRangeStartLabel: "pick start value",
-    defaultRangeEndLabel: "pick end value",
+    defaultRangeEndLabel  : "pick end value"
   },
-  table: {
-    emptyText: "No Data",
+  table      : {
+    emptyText    : "No Data",
     confirmFilter: "Confirm",
-    resetFilter: "Reset",
-    clearFilter: "All",
-    sumText: "Sum",
+    resetFilter  : "Reset",
+    clearFilter  : "All",
+    sumText      : "Sum"
   },
-  tree: {
-    emptyText: "No Data",
+  tree       : {
+    emptyText: "No Data"
   },
-  transfer: {
-    noMatch: "No matching data",
-    noData: "No data",
-    titles: ["List 1", "List 2"], // to be translated
+  transfer   : {
+    noMatch          : "No matching data",
+    noData           : "No data",
+    titles           : ["List 1", "List 2"], // to be translated
     filterPlaceholder: "Enter keyword", // to be translated
-    noCheckedFormat: "{total} items", // to be translated
-    hasCheckedFormat: "{checked}/{total} checked", // to be translated
+    noCheckedFormat  : "{total} items", // to be translated
+    hasCheckedFormat : "{checked}/{total} checked" // to be translated
   },
-  image: {
-    error: "FAILED",
+  image      : {
+    error: "FAILED"
   },
-  pageHeader: {
-    title: "Back", // to be translated
+  pageHeader : {
+    title: "Back" // to be translated
   },
-  popconfirm: {
+  popconfirm : {
     confirmButtonText: "Yes",
-    cancelButtonText: "No",
-  },
+    cancelButtonText : "No"
+  }
 };
--- client/src/lang/viVN.js
+++ client/src/lang/viVN.js
@@ -1,85 +1,104 @@
 export default {
-  auth: {
-    login: "Đăng nhập",
-    logout: "Đăng xuất",
-    register: "Đăng ký",
-    loginError: "Đăng nhập không thành công",
-    loginSuccess: "Đăng nhập thành công",
-    logoutError: "Đăng xuất không thành công",
-    logoutSuccess: "Bạn đã đăng xuất",
-    registerError: "Đăng ký không thành công",
-    registerSuccess: "Đăng ký thành công",
+  auth       : {
+    login           : "Đăng nhập",
+    logout          : "Đăng xuất",
+    register        : "Đăng ký",
+    loginError      : "Đăng nhập không thành công",
+    loginSuccess    : "Đăng nhập thành công",
+    logoutError     : "Đăng xuất không thành công",
+    logoutSuccess   : "Bạn đã đăng xuất",
+    registerError   : "Đăng ký không thành công",
+    registerSuccess : "Đăng ký thành công",
+    haveAnAccount   : "Bạn đã có tài khoản?",
+    haveNotAnAccount: "Bạn chưa có tài khoản?"
   },
-  pages: {
-    home: "Trang chủ",
+  pages      : {
+    home       : "Trang chủ",
     welcomeBack: "Xin chào",
-    getStarted: "Bắt đầu"
+    getStarted : "Bắt đầu",
+    noTasks    : "Không có nhiệm vụ nào cần làm!"
   },
-  form: {
-    name: "Họ và tên",
-    email: "Email",
-    password: "Mật khẩu",
+  carousel   : {
+    prev     : "Ảnh tiếp theo",
+    next     : "Ảnh trước",
+    ariaLabel: {
+      delimiter: "Carousel slide {0} trên {1}"
+    }
+  },
+  rating     : {
+    ariaLabel: {
+      icon: "Đánh giá {0} trên {1}"
+    }
+  },
+  form       : {
+    name                : "Họ và tên",
+    email               : "Email",
+    password            : "Mật khẩu",
     passwordConfirmation: "Nhập lại mật khẩu",
-    rememberMe: "Ghi nhớ mật khẩu",
+    rememberMe          : "Ghi nhớ mật khẩu"
   },
-  validate: {
-    required: "Đây là một trường bắt buộc",
-    email: "Vui lòng nhập đúng email",
-    min: "Cần nhập tối thiểu {0} kí tự",
-    confirmation: "{type} xác nhận không chính xác",
+  validate   : {
+    required    : "Đây là một trường bắt buộc",
+    email       : "Vui lòng nhập đúng email",
+    min         : "Cần nhập tối thiểu {0} kí tự",
+    confirmation: "{type} xác nhận không chính xác"
   },
-  network: {
+  network    : {
     connectSuccess: "Kết nối internet ổn định",
     connectFailure: "Kết nối internet không thành công",
-    connected: "Đã kết nối internet",
-    connecting: "Đang kết nối",
-    reconnecting: "@:network.connecting lại",
-    reconnected: "Đã khôi phục kết nối internet",
-    disconnected: "Ngắt kết nối",
-    offline: "Bạn đang offline",
+    connected     : "Đã kết nối internet",
+    connecting    : "Đang kết nối",
+    reconnecting  : "@:network.connecting lại",
+    reconnected   : "Đã khôi phục kết nối internet",
+    disconnected  : "Ngắt kết nối",
+    offline       : "Bạn đang offline"
   },
   colorpicker: {
     confirm: "OK",
-    clear: "Xóa",
+    clear  : "Xóa"
   },
-  datepicker: {
-    now: "Hiện tại",
-    today: "Hôm nay",
-    cancel: "Hủy",
-    clear: "Xóa",
-    confirm: "OK",
+  timePicker : {
+    am: "SA",
+    pm: "CH"
+  },
+  datepicker : {
+    now       : "Hiện tại",
+    today     : "Hôm nay",
+    cancel    : "Hủy",
+    clear     : "Xóa",
+    confirm   : "OK",
     selectDate: "Chọn ngày",
     selectTime: "Chọn giờ",
-    startDate: "Ngày bắt đầu",
-    startTime: "Thời gian bắt đầu",
-    endDate: "Ngày kết thúc",
-    endTime: "Thời gian kết thúc",
-    prevYear: "Năm trước",
-    nextYear: "Năm tới",
-    prevMonth: "Tháng trước",
-    nextMonth: "Tháng tới",
-    year: "Năm",
-    month1: "Tháng 1",
-    month2: "Tháng 2",
-    month3: "Tháng 3",
-    month4: "Tháng 4",
-    month5: "Tháng 5",
-    month6: "Tháng 6",
-    month7: "Tháng 7",
-    month8: "Tháng 8",
-    month9: "Tháng 9",
-    month10: "Tháng 10",
-    month11: "Tháng 11",
-    month12: "Tháng 12",
+    startDate : "Ngày bắt đầu",
+    startTime : "Thời gian bắt đầu",
+    endDate   : "Ngày kết thúc",
+    endTime   : "Thời gian kết thúc",
+    prevYear  : "Năm trước",
+    nextYear  : "Năm tới",
+    prevMonth : "Tháng trước",
+    nextMonth : "Tháng tới",
+    year      : "Năm",
+    month1    : "Tháng 1",
+    month2    : "Tháng 2",
+    month3    : "Tháng 3",
+    month4    : "Tháng 4",
+    month5    : "Tháng 5",
+    month6    : "Tháng 6",
+    month7    : "Tháng 7",
+    month8    : "Tháng 8",
+    month9    : "Tháng 9",
+    month10   : "Tháng 10",
+    month11   : "Tháng 11",
+    month12   : "Tháng 12",
     // week: 'week',
-    weeks: {
+    weeks : {
       sun: "CN",
       mon: "T2",
       tue: "T3",
       wed: "T4",
       thu: "T5",
       fri: "T6",
-      sat: "T7",
+      sat: "T7"
     },
     months: {
       jan: "Th.1",
@@ -93,65 +112,68 @@ export default {
       sep: "Th.9",
       oct: "Th.10",
       nov: "Th.11",
-      dec: "Th.12",
-    },
+      dec: "Th.12"
+    }
   },
-  select: {
-    loading: "Đang tải",
-    noMatch: "Dữ liệu không phù hợp",
-    noData: "Không tìm thấy dữ liệu",
+  select     : {
+    loading    : "Đang tải",
+    noMatch    : "Dữ liệu không phù hợp",
+    noData     : "Không tìm thấy dữ liệu",
+    placeholder: "Chọn"
+  },
+  cascader   : {
+    noMatch    : "Dữ liệu không phù hợp",
+    loading    : "Đang tải",
     placeholder: "Chọn",
+    noData     : "Không tìm thấy dữ liệu"
   },
-  cascader: {
-    noMatch: "Dữ liệu không phù hợp",
-    loading: "Đang tải",
-    placeholder: "Chọn",
-    noData: "Không tìm thấy dữ liệu",
-  },
-  pagination: {
-    goto: "Nhảy tới",
-    pagesize: "/trang",
-    total: "Tổng {total}",
+  pagination : {
+    goto          : "Đến trang",
+    pagesize      : "/trang",
+    total         : "Tổng {total}",
     pageClassifier: "",
+    currentPage   : "Trang hiện tại, Trang {0}"
   },
-  messagebox: {
-    title: "Thông báo",
+  messagebox : {
+    title  : "Thông báo",
     confirm: "OK",
-    cancel: "Hủy",
-    error: "Dữ liệu không hợp lệ",
+    cancel : "Hủy",
+    error  : "Dữ liệu không hợp lệ"
   },
-  upload: {
-    deleteTip: "Nhấn xoá để xoá",
-    delete: "Xóa",
-    preview: "Xem trước",
-    continue: "Tiếp tục",
+  upload     : {
+    deleteTip  : "Nhấn xoá để xoá",
+    delete     : "Xóa",
+    preview    : "Xem trước",
+    continue   : "Tiếp tục",
+    counter    : "{0} tệp",
+    counterSize: "{0} tệp (tổng cộng {1})"
   },
-  table: {
-    emptyText: "Không có dữ liệu",
+  table      : {
+    emptyText    : "Không có dữ liệu",
     confirmFilter: "Xác nhận",
-    resetFilter: "Làm mới",
-    clearFilter: "Xóa hết",
-    sumText: "Tổng",
+    resetFilter  : "Làm mới",
+    clearFilter  : "Xóa hết",
+    sumText      : "Tổng"
   },
-  tree: {
-    emptyText: "Không có dữ liệu",
+  tree       : {
+    emptyText: "Không có dữ liệu"
   },
-  transfer: {
-    noMatch: "Dữ liệu không phù hợp",
-    noData: "Không tìm thấy dữ liệu",
-    titles: ["Danh sách 1", "Danh sách 2"],
+  transfer   : {
+    noMatch          : "Dữ liệu không phù hợp",
+    noData           : "Không tìm thấy dữ liệu",
+    titles           : ["Danh sách 1", "Danh sách 2"],
     filterPlaceholder: "Nhập từ khóa",
-    noCheckedFormat: "{total} mục",
-    hasCheckedFormat: "{checked}/{total} đã chọn ",
+    noCheckedFormat  : "{total} mục",
+    hasCheckedFormat : "{checked}/{total} đã chọn "
   },
-  image: {
-    error: "LỖI",
+  image      : {
+    error: "LỖI"
   },
-  pageHeader: {
-    title: "Quay lại",
+  pageHeader : {
+    title: "Quay lại"
   },
-  popconfirm: {
+  popconfirm : {
     confirmButtonText: "Ok",
-    cancelButtonText: "Huỷ",
-  },
+    cancelButtonText : "Huỷ"
+  }
 };
