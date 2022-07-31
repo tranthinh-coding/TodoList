1938f40276e79c7ff2674d40c30344625ae4277f
Thinh <73821256+tranthinh-coding@users.noreply.github.com>
2022-08-01 00:03:08 +0700

    remove vuetify ...

--- client/src/main.js
+++ client/src/main.js
@@ -1,36 +1,33 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
+import App from "@/App.vue";
+import "@/registerServiceWorker";
+import router from "@/router";
 import { createPinia } from "pinia";
-
-/**
- * Reset Css
- */
-import "@/assets/style/reset.css";
-
-/**
- * Element plus
- */
+/** Element plus: vue element */
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
-import 'element-plus/theme-chalk/display.css'
+import "element-plus/theme-chalk/display.css";
 import "element-plus/theme-chalk/dark/css-vars.css";
-
-/**
- * NProgress Load Page
- */
+/** NProgress Load Page */
 import "nprogress/nprogress.css";
+/** Vue I18n  */
+import i18n from "@/plugins/i18n";
 
+/** Css */
+import "@/assets/style/index.css";
+/** Boxicons */
+import "@/assets/style/boxicons-2.1.2/css/boxicons.min.css";
 /**
- * Vue I18n
+ * Vuesax UI
  */
-import i18n from "@/config/i18n";
+import Vuesax from "@/plugins/vuesax";
 
 const app = createApp(App);
-
+/** Plugins */
+app.use(createPinia());
 app.use(ElementPlus);
 app.use(router);
+app.use(Vuesax);
 app.use(i18n);
-app.use(createPinia());
+
 app.mount("#app");
