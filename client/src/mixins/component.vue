df0cbf7cf37d92a05090423f4663c3b83191f578
Thinh <73821256+tranthinh-coding@users.noreply.github.com>
2022-07-31 23:57:58 +0700

    define global component for Vuesax package

---
+++ client/src/mixins/component.vue
@@ -0,0 +1,52 @@
+<script>
+import { defineComponent } from "vue"
+import { getColor } from "@/utils"
+
+export default defineComponent({
+  props: {
+    color: {
+      type: String,
+      default: null,
+    },
+    danger: {
+      type: Boolean,
+      default: false
+    },
+    success: {
+      type: Boolean,
+      default: false
+    },
+    warn: {
+      type: Boolean,
+      default: false
+    },
+    dark: {
+      type: Boolean,
+      default: false
+    },
+    primary: {
+      type: Boolean,
+      default: false
+    },
+    active: {
+      type: Boolean,
+      default: false
+    },
+  },
+  data: () => ({
+    componentColor: null,
+    getColor: null,
+  }),
+  computed: {
+    isColorDark() {
+      return this.color === 'dark' || this.dark || this.componentColor === 'dark'
+    },
+    isColor() {
+      return !!this.color || !!this.primary || !!this.success || !!this.warn || !!this.danger || !!this.dark
+    }
+  },
+  mounted() {
+    this.getColor = getColor(this.color)
+  }
+});
+</script>
