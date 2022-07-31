e2cc0babe8bdb21cbe6948edea8c3c6861434879
Thinh <73821256+tranthinh-coding@users.noreply.github.com>
2022-07-31 23:58:56 +0700

    change env api for API_V1

--- client/src/config/app.js
+++ client/src/config/app.js
@@ -1,7 +1,6 @@
-export const API_V1 = process.env.API_V1;
+export const API_V1 = process.env.VUE_APP_API_V1;
 export const ACCESS_TOKEN = "access_token";
 export const REMEMBER_TOKEN = "remember_token"; // remember_token
 export const REGISTER_API = "/register";
 export const LOGIN_API = "/login";
 export const LOGOUT_API = "/logout";
-export const nameOf = (f) => (f).toString().replace(/[ |\(\)=>]/g,''); // name of variable
