99918bb6d63d0ad3c84ed780c953b95cdaf36f5c
Thinh <73821256+tranthinh-coding@users.noreply.github.com>
2022-07-31 23:56:18 +0700

    fix logic router.beforeEach, auth and handle login

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,74 +3,72 @@ import { lazyViews } from "@/utils";
 import { useUser } from "@/store/useUser";
 import { ACCESS_TOKEN, REMEMBER_TOKEN } from "@/config/app";
 import { authRoutes } from "@/router/auth";
-import { getToken } from "@/utils/auth";
+import { getToken, removeToken } from "@/utils/auth";
 
 import DashboardLayout from "@/layouts/DashboardLayout.vue";
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: lazyViews('HomeView.vue'),
-    meta: {
+    path     : "/",
+    name     : "home",
+    component: lazyViews("HomeView.vue"),
+    meta     : {
       requireAuth: true,
-      title: "Home",
-      layout: DashboardLayout
-    },
+      title      : "Home",
+      layout     : DashboardLayout
+    }
   },
   {
-    path: "/about",
-    name: "about",
+    path     : "/about",
+    name     : "about",
     component: lazyViews("AboutView.vue"),
-    meta: {
+    meta     : {
       requireAuth: true,
-      title: "About",
-      layout: DashboardLayout
-    },
+      title      : "About",
+      layout     : DashboardLayout
+    }
   },
-  ...authRoutes,
+  ...authRoutes
 ];
 
-export const routerExceptedAuth = ["login", "register"];
-export const routerAuthUser = [...routerExceptedAuth];
+export const routerAuthUser     = ["login", "register"];
+export const routerExceptedAuth = [...routerAuthUser];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-  scrollBehavior(to, _, savePosition) {
-    if (savePosition) {
-      return savePosition;
-    }
+  scrollBehavior (to, _, savedPosition) {
+    let scrollTo = 0;
     if (to.hash) {
-      return to.hash;
+      scrollTo = to.hash;
+    } else if (savedPosition) {
+      scrollTo = savedPosition.y;
     }
-
-    return { top: 0 };
-  },
+    return { y: scrollTo, behavior: "smooth" };
+  }
 });
 
-router.beforeEach((to, _, next) => {
+router.beforeEach(async (to, _, next) => {
+  if (to.name === "logout") return next();
   const user = useUser();
-  // user not logged in
-  if (!user.getLoginStatus(ACCESS_TOKEN)) {
-    // check user has remember me token
+  if ( ! user.getLoginStatus(ACCESS_TOKEN)) {
+    // user was not logged in
     const hasRememberToken = getToken(REMEMBER_TOKEN);
-    if (hasRememberToken !== "undefined") {
-      user.login({
+    if (hasRememberToken) {
+      const { error } = await user.login({
         [REMEMBER_TOKEN]: hasRememberToken,
-        device_name: ACCESS_TOKEN,
+        device_name     : ACCESS_TOKEN
       });
-      console.log("Logged with remember token");
-      return next();
+      if ( ! error.value) {
+        if ( ! routerAuthUser.includes(to.name)) return next();
+        return next({ name: "home" });
+      }
+      removeToken([REMEMBER_TOKEN, ACCESS_TOKEN]);
     }
-    // not has remember token: check to.route required Auth? or this route was excepted auth
-    if (!to.meta.requireAuth || routerExceptedAuth.includes(to.name)) {
-      return next();
-    }
-    // to.route required Auth => redirect login view
+    if ( ! to.meta.requireAuth || routerExceptedAuth.includes(to.name)) return next();
     return next({ name: "login" });
   }
-  // was logged in.
+  if (routerAuthUser.includes(to.name)) return next({ name: "home" });
   return next();
 });
 
