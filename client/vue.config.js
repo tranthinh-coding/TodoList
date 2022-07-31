b2046ce058a744f7adf1efed40df571cd0a9d87d
Thinh <73821256+tranthinh-coding@users.noreply.github.com>
2022-07-31 23:46:34 +0700

    add some options

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  assetsDir: 'src/assets',
+  pwa: {
+    name: 'Todo List',
+    manifestOptions: {
+      background_color: '#070606'
+    }
+  }
 });
